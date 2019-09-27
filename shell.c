ca372d2efff6924aa22a34ae0f2c5dd40b965bdc
daelynj <daelyndbjones@gmail.com>
2019-09-26 21:04:44 -0700

    Implement background task and bglist
    This took many, many hours. Why is C so frustrating? Why can't I just
    concatenate strings like string1 + string2 please.

--- shell.c
+++ shell.c
@@ -10,6 +10,66 @@
 #define HOSTNAME_MAX 1024
 #define INPUT_SIZE_MAX 128
 
+struct bg_pro{
+    pid_t pid;
+    char command;
+    struct bg_pro *next;
+};
+
+struct bg_pro *bg_pro_list_head = NULL;
+struct bg_pro *bg_pro_list_tail = NULL;
+
+void delete_process_from_list(pid_t pid) {
+  struct bg_pro *i = bg_pro_list_head;
+  struct bg_pro *j = NULL;
+
+  while (i != NULL) {
+    if (i->pid == pid) {
+      if (i == bg_pro_list_head) {
+        bg_pro_list_head = bg_pro_list_head->next;
+      } else {
+        j->next = i->next;
+      }
+      free(i);
+      return;
+    }
+    j = i;
+    i = i->next;
+  }
+}
+
+void add_process_to_list(pid_t pid, char **user_input) {
+  struct bg_pro *new_process = malloc(sizeof(struct bg_pro));
+
+  new_process->pid = pid;
+  new_process->command = *user_input[1];
+  new_process->next = NULL;
+
+  if (bg_pro_list_head == NULL) {
+    bg_pro_list_head = new_process;
+    bg_pro_list_tail = new_process;
+  } else {
+    bg_pro_list_tail->next = new_process;
+		bg_pro_list_tail = new_process;
+  }
+}
+
+void check_background_processes() {
+	pid_t pid;
+	int	status;
+
+	while (true) {
+		pid = waitpid(-1, &status, WNOHANG);
+
+		if (pid > 0 && WIFEXITED(status)) {
+			printf("\nBackground process %d has terminated.\n", pid);
+      delete_process_from_list(pid);
+		} else {
+			break;
+		}
+	}
+}
+
 void print_input_field() {
   char hostname[HOSTNAME_MAX];
   gethostname(hostname, HOSTNAME_MAX);
@@ -29,12 +89,12 @@ void get_user_input(char **user_input) {
 
   char *raw_user_input = readline(" ");
 
-  char *token = strtok(raw_user_input, " ");
+  char *current_token = strtok(raw_user_input, " ");
 
   int i;
   for (i = 0; i < INPUT_SIZE_MAX; i++) {
-    user_input[i] = token;
-    token = strtok(NULL, " ");
+    user_input[i] = current_token;
+    current_token = strtok(NULL, " ");
   }
 }
 
@@ -57,6 +117,7 @@ void execute_builtin_commands(char **user_input) {
     int execution_condition = execvp(user_input[0], user_input);
     if (execution_condition == -1) {
       printf("invalid command\n");
+      exit(1);
     }
   } else if (pid < 0) {
     printf("forking error\n");
@@ -69,20 +130,45 @@ void execute_builtin_commands(char **user_input) {
 }
 
 void execute_background_task(char **user_input) {
-  //
-  //
-  //
+  if (user_input[1] == NULL) {
+    printf("invalid command\n");
+    return;
+  }
+  
+	pid_t pid = fork();
+
+	if (pid == 0) {
+    // this is in the child process
+		int execution_condition = execvp(user_input[1], &user_input[1]);
+    if (execution_condition == -1) {
+      printf("invalid command\n");
+      exit(1);
+    }
+	} else if (pid < 0) {
+    printf("forking error\n");
+  } else {
+    // this is in the parent process
+		printf("New background process: %d\n", pid);
+    add_process_to_list(pid, user_input);
+    usleep(5000);
+	}
 }
 
 void print_background_task_list() {
-  //
-  //
-  //
+  struct bg_pro *i = bg_pro_list_head;
+
+	int j = 0;
+	while (i != NULL) {
+    j++;
+    printf("%d: %c\n", i->pid, i->command);
+		i = i->next;
+  }
+  printf("Total Background jobs: %d\n", j);
 }
 
 void execute_user_input(char **user_input) {
   if (!strcmp(user_input[0], "quit")) {
-    exit(false);
+    exit(0);
   } else if (!strcmp(user_input[0], "cd")) {
     change_directories(user_input);
   } else if (!strcmp(user_input[0], "bg")) {
@@ -100,59 +186,8 @@ int main() {
 
     get_user_input(user_input);
     execute_user_input(user_input);
+    check_background_processes();
   }
 
   return true;
 }
-
-// PART 2
-
-// if strcmp(argv[0], "cd") == 0 PART 2
-// else PART 1
-
-// if argv[1] == NULL || strcmp(argv[1], "~") == 0 ---> go to HOME (env)
-// else go to argv[1]
-
-// "env | grep HOME" in BASH to verify
-
-// PART 3
-
-// strcmp(argv[0], "bg") == 0
-// new argv: remove "bg"
-// fork:
-//   child: execvp(newArgv[0], newArgv)
-//     parent:
-//       if # of bg_process == 0
-//        root = bg_pro1
-//       else { append bg_proNext }
-
-// should use a linked list  for storing bg process info, with add/delete etc.
-
-// struct bg_pro {
-//   pid t pid;
-//   char command[1024];
-//   struct bg_pro* next; // pointer to the next structure, or a \0
-// }
-// root should point to a structure.. should point to a structure..  where each
-// struct is a process
-
-// check if child terminates
-// if # of bg_process > 0
-//   pid_t ter = waitpid(0, NULL, WNOHANG);
-//   while ter > 0 {
-//     if root = pid -> ter
-//       print root -> pid & root -> command & "has terminated"
-//       root = root -> next
-//     else
-//       loop until we find cur -> next -> pid == ter
-//       print cur -> next -> pid & cur -> next -> command & "has terminated"
-//       cur -> next = cur -> next -> next
-//     ter = waitpid(0, NULL, WNOHANG);
-//   }
-
-// bglist
-// strcmp(argv[0], "bglist") == 0;
-// print info in the linked list and number of bg processes
-
-// bg ls -l == ls -l &
-//
