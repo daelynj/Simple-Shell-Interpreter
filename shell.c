b7588bbbcb83bc2033a5987a824e2079a1d8b5cb
daelynj <daelyndbjones@gmail.com>
2019-09-30 13:47:42 -0700

    Add libraries

--- shell.c
+++ shell.c
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 // This isn't the stone age, thus I opt to redefine 1 and 0 as true and false.
